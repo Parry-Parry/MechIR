d96ad477768f3cfc8c1df7d702599968cc10dcfb
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-07-21 01:57:23 +0100

    adding HookedEncoderForClassification + linear

--- src/mechir/data/dataset.py
+++ src/mechir/data/dataset.py
@@ -4,32 +4,24 @@ import torch
 import ir_datasets as irds
 
 class PairDataset(Dataset):
-    def __init__(self, 
-                 pairs : pd.DataFrame, 
+    def __init__(self,  
                  ir_dataset : str,
+                 pairs : pd.DataFrame = None,
                  ) -> None:
         super().__init__()
-        self.pairs = pairs
+        self.ir_dataset = irds.load(ir_dataset)
+        self.qrels = pd.DataFrame(self.ir_dataset.qrels_iter()).set_index("query_id")
+        self.pairs = pairs if pairs is not None else pd.DataFrame(self.ir_dataset.qrels_iter())
         for column in 'query_id', 'doc_id':
             if column not in self.pairs.columns: raise ValueError(f"Format not recognised, Column '{column}' not found in pairs dataframe")
-        self.ir_dataset = irds.load(ir_dataset)
         self.docs = pd.DataFrame(self.ir_dataset.docs_iter()).set_index("doc_id")["text"].to_dict()
         self.queries = pd.DataFrame(self.ir_dataset.queries_iter()).set_index("query_id")["text"].to_dict()
 
-        self.qrels = pd.DataFrame(self.ir_dataset.qrels_iter()).set_index("query_id")
+        
 
-    '''
-    @classmethod
-    def from_irds_qrels(cls,
-                    ir_dataset : str,
-                    group_size : int = 2,
-                    ) -> 'PairDataset':
-            pairs = initialise_pairs(ir_dataset)
-            return cls(pairs, ir_dataset, teacher_file, group_size)
-    '''
     def __len__(self):
         return len(self.pairs)
 
     def __getitem__(self, idx):
         item = self.pairs.iloc[idx]
-        return item['query_id'], item['doc_id']
+        return self.queries[item['query_id']], self.docs[item['doc_id']]
