a448ebf6cefdeecee005d7aaabd009192dd78b51
Andrew <2393265p@student.gla.ac.uk>
2024-07-27 12:51:59 +0100

    make skeleton for proximity

--- src/mechir/perturb/axiom/prox.py
+++ src/mechir/perturb/axiom/prox.py
@@ -5,35 +5,8 @@ from .. import AbstractPerturbation
 
 class ProximityPerturbation(AbstractPerturbation):
     def __init__(self, 
-                 index_location: Any | Path | str, 
-                 dataset: Any | str | None = None, 
-                 contents_accessor: str | Callable[[NamedTuple], str] | None = "text", 
-                 tokeniser: Any | None = None, 
-                 cache_dir: Path | None = None
                  ) -> None:
-        super().__init__(index_location, dataset, contents_accessor, tokeniser, cache_dir)
-    
-    def _insert_terms(self, text : str, terms : list[str]) -> str:
-        if self.loc == 'end':
-            return f"{text} {' '.join(terms)}"
-        elif self.loc == 'start':
-            return f"{' '.join(terms)} {text}"
-        else:
-            raise ValueError(f"Invalid loc value {self.loc}")
-    
-    def _get_top_k_freq_terms(self, text : str) -> dict:  
-        freq = self.get_freq_text(text)
-        # Get the top num_additions terms with the highest term frequency
-        return sorted(freq.items(), key=lambda x: x[1], reverse=True)[:self.num_additions]
-
-    def _get_max_freq_terms(self, text : str) -> int:
-        freq = self.get_freq_text(text)
-        term = max(freq, key=freq.get)
-        return [freq[term]] * self.num_additions
-    
-    def _get_min_freq_terms(self, text : str) -> int:
-        freq = self.get_freq_text(text)
-        term = min(freq, key=freq.get)
-        return [freq[term]] * self.num_additions
-
-    def apply(self, document : str
+        super().__init__()
+    def apply(self, document : str, query : str) -> str:
+        terms = self.get_freq_terms(query if self.target=='query'else document)
+        return self._insert_terms(document, terms
