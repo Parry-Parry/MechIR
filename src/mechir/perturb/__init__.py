1ff7214760b0d04cc4a6d14066816260bf3e9d58
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-09-12 15:20:35 +0100

    allow easy instantiation

--- src/mechir/perturb/__init__.py
+++ src/mechir/perturb/__init__.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from functools import wraps
 from pyterrier.datasets import IRDSDataset as _IRDSDataset
 IRDSDataset = _IRDSDataset
 
@@ -13,5 +14,11 @@ class AbstractPerturbation(object, ABC):
 class IdentityPerturbation(AbstractPerturbation):
     def apply(self, document : str, query : str = None) -> str:
         return document
+    
+def perturbation(f):
+    class CustomPerturbation(AbstractPerturbation):
+        def apply(self, document: str, query: str = None) -> str:
+            return f(document, query)
+    return CustomPerturbation()
     
 from .index import IndexPerturbation
