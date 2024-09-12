1bc817a013bb2968da05e116abc1aba08a848547
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-09-12 15:46:36 +0100

    add optional pair path

--- examples/cat.py
+++ examples/cat.py
@@ -1,14 +1,15 @@
 from mechir import Cat, PairDataset, CatDataCollator, perturbation
 from torch.data import DataLoader
+import pandas as pd
 import json
 
 @perturbation
 def perturb(self, text : str, query : str = None) -> str:
     return text + "Apple"
 
-def patch_cat(model_name_or_path : str, out_path : str, dataset : str, num_labels : int = 2, batch_size : int = 1, patch_type : str = 'block_all', block_list : str = None):
+def patch_cat(model_name_or_path : str, out_path : str, dataset : str, num_labels : int = 2, batch_size : int = 1, patch_type : str = 'block_all', block_list : str = None, pair_path : str = None):
     
-
+    pairs = pd.read_csv(pair_path) if pair_path is not None else None
     
     patch_args = {
         'patch_type' : patch_type,
@@ -18,9 +19,8 @@ def patch_cat(model_name_or_path : str, out_path : str, dataset : str, num_label
         assert block_list is not None, "block_list must be provided for by_pos patching"
         patch_args['block_list'] = block_list
 
-
     model = Cat(model_name_or_path, num_labels)
-    dataset = PairDataset(dataset)
+    dataset = PairDataset(dataset, pairs)
     data_collator = CatDataCollator(model.tokenizer, perturb)
 
     dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=data_collator)
