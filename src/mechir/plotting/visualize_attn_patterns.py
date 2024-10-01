b0ea7cc06b32e53397801ee0aaa31270225dbb41
Catherine Chen <catherine_s_chen@brown.edu>
2024-10-01 14:03:28 -0400

    fix streamlit page for visualizing attention patterns

--- src/mechir/plotting/visualize_attn_patterns.py
+++ src/mechir/plotting/visualize_attn_patterns.py
@@ -2,7 +2,6 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
-import plotly.figure_factory as ff
 
 st.set_page_config(
     page_title="Visualize Attention Patterns",
@@ -11,20 +10,19 @@ st.set_page_config(
 
 
 def load_doc(fname):
+    with open(fname, "r") as f:
+        text = f.read()
+    return text.split()
 
-    return []
 
-
-'''
-Plots and interactive plotly graph to visualize the attention pattern for a single document.
-'''
+# Plots and interactive plotly graph to visualize the attention pattern for a single document.
 def plot_attn_for_doc(attn_pattern, doc_tokens):
 
     fig = go.Figure(data=go.Heatmap(z=attn_pattern,colorscale='RdBu', zmin=-1, zmax=1))
     fig.update_layout(
             title='Attention Pattern',
             xaxis=dict(tickvals=np.arange(len(doc_tokens)),ticktext=doc_tokens, title="Source"),
-            yaxis=dict(tickvals=np.arange(len(doc_tokens)),ticktext=doc_tokens[::-1], title="Destination"),
+            yaxis=dict(tickvals=np.arange(len(doc_tokens)),ticktext=doc_tokens, title="Destination"),
         )
 
     return fig
@@ -33,21 +31,20 @@ def plot_attn_for_doc(attn_pattern, doc_tokens):
 
 ###################### Start page #########################
 
-st.markdown("# Homepage")
-st.sidebar.markdown("# Homepage")
+st.markdown("# Visualize Attention Patterns")
 
 
 # Minimal example on how to visualize attention patterns for a single document
 
-# (1) Load data
-attn_pattern_fname = ""
+# (1) Load attention pattern
+attn_pattern_fname = "" # FILL IN WITH THE PATH TO THE ATTENTION PATTERN FILE
 data = np.load(attn_pattern_fname)
 
-# TODO: load document tokens
-doc_fname = ""
+# (2) Load document text
+doc_fname = "" # FILL IN WITH THE PATH TO YOUR FILE
 doc_tokens = load_doc(doc_fname)
 
-# (2) Plot
+# (3) Plot
 fig = plot_attn_for_doc(data, doc_tokens)
 st.plotly_chart(fig)
 
