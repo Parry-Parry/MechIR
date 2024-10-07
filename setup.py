f5802dbe8e0f2c6c7ac132a57d8a49d16835142f
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-10-07 14:33:23 +0100

    add requirements.txt

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     description='A default python template',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    requires=open('requirements.txt').read().splitlines(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
