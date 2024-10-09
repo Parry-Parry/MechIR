9ad73fad6c01fcc54b58feae9f6f14b0a0f545d9
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-10-09 16:18:12 +0100

    standardise padding

--- src/mechir/data/loader/t5.py
+++ src/mechir/data/loader/t5.py
@@ -6,11 +6,14 @@ class MonoT5DataCollator:
                  transformation_func : callable,
                  q_max_length=30,
                  d_max_length=200,
+                 special_token="X",
                  ) -> None:
         self.tokenizer = tokenizer
         self.transformation_func = transformation_func
         self.q_max_length = q_max_length
         self.d_max_length = d_max_length
+        self.special_token = special_token
+        self.special_token_id = self.tokenizer.convert_tokens_to_ids(self.special_token)
 
     def prompt(query : str, document : str):
         return f"query: {query} document: {document} relevant:"
@@ -27,7 +30,7 @@ class MonoT5DataCollator:
             batch_scores.extend(args[0])
         
         batch_perturbed_docs = [self.transformation_func(dx, query=q) for q, dx in zip(batch_queries, batch_docs)]
-        batch_docs, batch_perturbed_docs = zip(*[pad(a, b, self.special_token) for a, b in zip(batch_docs, batch_perturbed_docs)])
+        batch_docs = [pad(a, b, self.special_token) for a, b in zip(batch_docs, batch_perturbed_docs)]
 
         tokenized_sequences = self.tokenizer(
             [self.prompt(q, dx) for q, dx in zip(batch_queries, batch_docs)],
