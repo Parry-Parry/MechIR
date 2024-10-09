5f9a104bf131b66b7e17e4e24a19506c017f25d6
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-10-09 16:44:53 +0100

    try fix

--- src/mechir/perturb/__init__.py
+++ src/mechir/perturb/__init__.py
@@ -1,3 +1,5 @@
+# __init__.py
+
 from abc import ABC, abstractmethod
 from typing import Any, TYPE_CHECKING
 from ..util import is_ir_axioms_availible
@@ -5,30 +7,28 @@ from transformers.utils import _LazyModule, OptionalDependencyNotAvailable
 
 class AbstractPerturbation(ABC):
     @abstractmethod
-    def apply(self, document : str, query : str) -> str:
+    def apply(self, document: str, query: str) -> str:
         raise NotImplementedError("This method should be implemented in the subclass")
     
-    def __call__(self, document : str, query : str = None) -> str:
+    def __call__(self, document: str, query: str = None) -> str:
         return self.apply(document, query)
 
 class IdentityPerturbation(AbstractPerturbation):
-    def apply(self, document : str, query : str = None) -> str:
+    def apply(self, document: str, query: str = None) -> str:
         return document
-    
+
 def perturbation(f):
     argcount = f.__code__.co_argcount
     class CustomPerturbation(AbstractPerturbation):
         def apply(self, document: str, query: str = None) -> str:
             return f(document, query) if argcount > 1 else f(document)
+    return CustomPerturbation()
 
-    instance = CustomPerturbation()
-    return instance
+# Define these classes and function in the current namespace
+__all__ = ["AbstractPerturbation", "IdentityPerturbation", "perturbation"]
 
 _import_structure = {
-    'AbstractPerturbation' : ['AbstractPerturbation'],
-    'IdentityPerturbation' : ['IdentityPerturbation'],
-    'perturbation' : ['perturbation'],
-    'IRDSDataset' : ['IRDSDataset'],
+    "IRDSDataset": ["IRDSDataset"],
 }
 
 try:
@@ -41,7 +41,6 @@ else:
     from .axiom import __all__ as _axiom_all
     _import_structure["index"] = _index_all
     _import_structure["axiom"] = _axiom_all
-    
 
 if TYPE_CHECKING:
     try:
@@ -53,5 +52,8 @@ if TYPE_CHECKING:
         from .index import *
         from .axiom import *
 else:
-    import sys 
+    import sys
     sys.modules[__name__] = _LazyModule(__name__, globals()["__file__"], _import_structure, module_spec=__spec__)
+
+# Update __all__ to include both directly defined and lazily loaded items
+__all__ += list(_import_structure.keys())
