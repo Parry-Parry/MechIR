6fd0129e300ee7c01612d4c3df8f3c7c130e75bc
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-10-15 16:17:15 +0100

    should fix

--- src/mechir/modelling/hooked/loading_from_pretrained.py
+++ src/mechir/modelling/hooked/loading_from_pretrained.py
@@ -131,6 +131,8 @@ def get_official_model_name(model_name: str):
     """
     Returns the official model name for a given model name (or alias).
     """
+    if model_name in OFFICIAL_MODEL_NAMES:
+        return model_name
     model_alias_map = make_model_alias_map()
     official_model_name = model_alias_map.get(model_name.lower(), None)
     if official_model_name is None:
