0dc08bf2c612ed57d4b1cefc2394335b2226d69a
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-10-15 16:22:53 +0100

    should fix, wrong import

--- src/mechir/modelling/hooked/HookedDistilBert.py
+++ src/mechir/modelling/hooked/HookedDistilBert.py
@@ -15,12 +15,11 @@ from torch import nn
 from transformers import AutoTokenizer
 from typing_extensions import Literal
 
-import transformer_lens.loading_from_pretrained as loading
 from transformer_lens import ActivationCache, FactoredMatrix, HookedTransformerConfig
 from transformer_lens.components import BertBlock, BertEmbed, BertMLMHead, Unembed
 from transformer_lens.hook_points import HookedRootModule, HookPoint
 from transformer_lens.utilities import devices
-
+from . import loading_from_pretrained as loading
 
 class HookedDistilBert(HookedRootModule):
     def __init__(self, cfg, tokenizer=None, move_to_device=True, **kwargs):
--- src/mechir/modelling/hooked/HookedEncoderForSequenceClassification.py
+++ src/mechir/modelling/hooked/HookedEncoderForSequenceClassification.py
@@ -14,10 +14,11 @@ from jaxtyping import Float, Int
 from torch import nn
 from typing_extensions import Literal
 
-import transformer_lens.loading_from_pretrained as loading
 from transformer_lens.ActivationCache import ActivationCache
 from transformer_lens import HookedEncoder
 from .linear import Linear
+from . import loading_from_pretrained as loading
+
 
 
 class HookedEncoderForSequenceClassification(HookedEncoder):
