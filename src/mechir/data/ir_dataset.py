b60f6f3cbd6981176909ee8040b7b17ef5693915
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-10-18 11:48:45 +0100

    allow lazy loading

--- src/mechir/data/ir_dataset.py
+++ src/mechir/data/ir_dataset.py
@@ -6,6 +6,7 @@ class MechIRDataset(Dataset):
     def __init__(self,  
                  ir_dataset : str,
                  pairs : pd.DataFrame = None,
+                 lazy_load_docs : bool = False    
                  ) -> None:
         super().__init__()
         self.ir_dataset = irds.load(ir_dataset)
@@ -13,14 +14,23 @@ class MechIRDataset(Dataset):
         self.pairs = pairs if pairs is not None else pd.DataFrame(self.ir_dataset.qrels_iter())
         for column in 'query_id', 'doc_id':
             if column not in self.pairs.columns: raise ValueError(f"Format not recognised, Column '{column}' not found in pairs dataframe")
-        self.docs = pd.DataFrame(self.ir_dataset.docs_iter()).set_index("doc_id")["text"].to_dict()
+        self.lazy_load_docs = lazy_load_docs
+        if self.lazy_load_docs: self.docs = self.ir_dataset.docs_store()
+        else: self.docs = pd.DataFrame(self.ir_dataset.docs_iter()).set_index("doc_id")["text"].to_dict()
         self.queries = pd.DataFrame(self.ir_dataset.queries_iter()).set_index("query_id")["text"].to_dict()
+    
+    def _get_doc(self, doc_id):
+        if self.lazy_load_docs: return self.docs.get(doc_id)
+        else: return self.docs[doc_id]
+    
+    def _get_query(self, query_id):
+        return self.queries[query_id]
 
     def __len__(self):
         return len(self.pairs)
 
     def __getitem__(self, idx):
         item = self.pairs.iloc[idx]
-        return self.queries[item['query_id']], self.docs[item['doc_id']]
+        return self._get_query(item['query_id']), self._get_doc(item['doc_id'])
 
 __all__ = ["MechIRDataset"]
