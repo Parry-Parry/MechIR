2a02f870fde5886449751263b35f7118d54a27e2
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-10-19 13:25:38 +0100

    minor push for flexibility

--- src/mechir/data/ir_dataset.py
+++ src/mechir/data/ir_dataset.py
@@ -7,7 +7,8 @@ class MechIRDataset(Dataset):
                  ir_dataset : str,
                  pairs : pd.DataFrame = None,
                  lazy_load_docs : bool = False,
-                 text_field : str = "text"
+                 text_field : str = "text",
+                 query_field : str = "text",
                  ) -> None:
         super().__init__()
         self.ir_dataset = irds.load(ir_dataset)
@@ -17,9 +18,10 @@ class MechIRDataset(Dataset):
             if column not in self.pairs.columns: raise ValueError(f"Format not recognised, Column '{column}' not found in pairs dataframe")
         self.lazy_load_docs = lazy_load_docs
         self._text_field = text_field
+        self._query_field = query_field
         if self.lazy_load_docs: self.docs = self.ir_dataset.docs_store()
-        else: self.docs = pd.DataFrame(self.ir_dataset.docs_iter()).set_index("doc_id")["text"].to_dict()
-        self.queries = pd.DataFrame(self.ir_dataset.queries_iter()).set_index("query_id")["text"].to_dict()
+        else: self.docs = pd.DataFrame(self.ir_dataset.docs_iter()).set_index("doc_id")[self._text_field].to_dict()
+        self.queries = pd.DataFrame(self.ir_dataset.queries_iter()).set_index("query_id")[self._query_field].to_dict()
     
     def _get_doc(self, doc_id):
         if self.lazy_load_docs: return getattr(self.docs.get(doc_id), self._text_field)
