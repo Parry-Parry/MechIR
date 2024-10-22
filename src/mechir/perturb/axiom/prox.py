87e1cc10ba53b76a04fc9a6a07beb2f7c44df541
Andrew <2393265p@student.gla.ac.uk>
2024-10-22 12:42:16 +0100

    minor change

--- src/mechir/perturb/axiom/prox.py
+++ src/mechir/perturb/axiom/prox.py
@@ -2,8 +2,7 @@ from functools import partial
 from .. import AbstractPerturbation
 
 class ProximityPerturbation(AbstractPerturbation):
-    def __init__(self, 
-                 ) -> None:
+    def __init__(self) -> None:
         super().__init__()
     def apply(self, document : str, query : str) -> str:
         terms = self.get_freq_terms(query if self.target=='query' else document)
