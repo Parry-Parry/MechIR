c82a47f6c541061d346c94662c214de00d3c6e72
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-10-26 16:01:23 +0100

    fix t5

--- src/mechir/data/loader/t5.py
+++ src/mechir/data/loader/t5.py
@@ -4,37 +4,36 @@ class MonoT5DataCollator(BaseCollator):
     def __init__(self, 
                  tokenizer,
                  transformation_func : callable,
+                 special_mask=False,
+                 perturb_type='append',
                  q_max_length=30,
                  d_max_length=200,
                  special_token="X",
                  ) -> None:
-        super().__init__(tokenizer, transformation_func, q_max_length, d_max_length, special_token)
+        super().__init__(tokenizer, transformation_func, special_mask, perturb_type, q_max_length, d_max_length, special_token)
 
     def prompt(query : str, document : str):
         return f"query: {query} document: {document} relevant:"
 
     def __call__(self, batch) -> dict:
-        batch_queries = []
-        batch_docs = []
-        batch_scores = []
-        for (q, dx, *args) in batch:
-            batch_queries.extend([q]*len(dx))
-            batch_docs.extend(dx)
-            if len(args) == 0:
-                continue
-            batch_scores.extend(args[0])
-        
-        batch_perturbed_docs = [self.transformation_func(dx, query=q) for q, dx in zip(batch_queries, batch_docs)]
-        batch_docs = [self.pad(a, b, self.special_token) for a, b in zip(batch_docs, batch_perturbed_docs)]
+        batch_queries, batch_docs = zip(*batch)
+        batch_perturbed_docs = [self.transformation_func(dx, query=q) for q, dx in batch]
+
+        batch_padded_docs, batch_padded_perturbed_docs = [], []
+
+        for doc_a, doc_b in zip(batch_docs, batch_perturbed_docs):
+            padded_a, padded_b = self.pad_by_perturb_type(doc_a, doc_b)
+            batch_padded_docs.append(padded_a)
+            batch_padded_perturbed_docs.append(padded_b)
 
         tokenized_sequences = self.tokenizer(
-            [self.prompt(q, dx) for q, dx in zip(batch_queries, batch_docs)],
+            [self.prompt(q, dx) for q, dx in zip(batch_queries, batch_padded_docs)],
             padding='max_length',
             max_length=self.q_max_length + self.d_max_length,
             return_tensors="pt",
         )
         tokenized_perturbed_sequences = self.tokenizer(
-            [self.prompt(q, dx) for q, dx in zip(batch_queries, batch_perturbed_docs)],
+            [self.prompt(q, dx) for q, dx in zip(batch_queries, batch_padded_perturbed_docs)],
             padding='max_length',
             max_length=self.q_max_length + self.d_max_length,
             return_tensors="pt",
