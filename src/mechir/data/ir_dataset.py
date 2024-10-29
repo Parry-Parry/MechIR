6bd7311c76e0acc9c26962cc24f98932306a7560
Catherine Chen <catherine_s_chen@brown.edu>
2024-10-28 21:29:57 -0400

    small init change for mechir dataset for demo

--- src/mechir/data/ir_dataset.py
+++ src/mechir/data/ir_dataset.py
@@ -9,6 +9,7 @@ class MechIRDataset(Dataset):
                  lazy_load_docs : bool = False,
                  text_field : str = "text",
                  query_field : str = "text",
+                 query_id_subset: list = None,
                  ) -> None:
         super().__init__()
         self.ir_dataset = irds.load(ir_dataset)
@@ -16,6 +17,10 @@ class MechIRDataset(Dataset):
         self.pairs = pairs if pairs is not None else pd.DataFrame(self.ir_dataset.qrels_iter())
         for column in 'query_id', 'doc_id':
             if column not in self.pairs.columns: raise ValueError(f"Format not recognised, Column '{column}' not found in pairs dataframe")
+        
+        if query_id_subset is not None:
+            self.pairs = self.pairs[self.pairs['query_id'].isin(query_id_subset)]
+        
         self.lazy_load_docs = lazy_load_docs
         self._text_field = text_field
         self._query_field = query_field
