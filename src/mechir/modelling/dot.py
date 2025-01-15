c83967fafbce28a1886556b127e11c661ef5177b
“Andrew <a.parry.1@research.gla.ac.uk>
2025-01-15 14:26:56 +0000

    sanity

--- src/mechir/modelling/dot.py
+++ src/mechir/modelling/dot.py
@@ -138,6 +138,7 @@ class Dot(HookedRootModule, PatchedMixin, SAEMixin):
             corrupted_tokens=corrupted_tokens, clean_cache=clean_cache
         ):
             output = batched_dot_product(reps_q, output)
+            print(output.size())
             results[index] = patching_metric(output, scores, scores_p).mean()
 
         return results
