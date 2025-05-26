4985fb078cc773edbac5ce09ec644eb852400539
“Andrew <a.parry.1@research.gla.ac.uk>
2025-05-26 12:40:52 +0100

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'mechir'
 
 setup(
     name=package_name,  # The name of your package
-    version='0.0.2',  # Your package version
+    version='0.0.4',  # Your package version
     packages=find_packages(where='src'),  # Look for packages in the 'src' directory
     package_dir={'': 'src'},  # Map the package name to the 'src' directory
     author='Anon A. Mous',
